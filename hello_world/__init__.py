b1197a87fcbe4fd292785a87d38346c56c5aeca0
GreciaRuiz <ruizgreciagimena@gmail.com>
2023-09-25 22:45:51 -0300

    modificacion

--- hello_world/__init__.py
+++ hello_world/__init__.py
@@ -57,9 +57,10 @@ def init_app():
 
 	@app.route('/actors', methods= ['POST'])
 	def create_actor():
-		query= "INSERT INTO sakila.actor first_name, last_name, last_update"
+		query= "INSERT INTO sakila.actor (first_name, last_name, last_update) VALUES (%s,%s,%s);"
+		params= request.args.get('first_name', ''), request.args.get('last_name', ''),
 		request.args.get('last_update', '')
-		DatabaseConnection.execute_query (query,params)
+		DatabaseConnection.execute_query (query, params)
 		return {"msg": "Actor creado con éxito"}, 201
-		
+
 	return app
